e093c029946c2cbfaa662e41eb6fb3da4888b0b7
Dinesh PR <58638612+dinesh110598@users.noreply.github.com>
2021-01-02 19:47:57 +0530

    Updated to include external magnetic fields

--- Ising_kernels.py
+++ Ising_kernels.py
@@ -29,7 +29,7 @@ def update_black (spin, seed, T, J):
 
     def calc():
         probs = random_uniform()
-        if (probs < math.exp(2*J[0]*spin[z, x, y]*sum_nn()/T[n,l])):
+        if (probs < math.exp(2*(J[0]*sum_nn())*spin[z, x, y]/T[n,l])):
             spin[z, x, y] *= np.int8(-1)
 
     if (p == 0 and q == 0) or (p == 1 and q == 1):
@@ -62,7 +62,7 @@ def update_white (spin, seed, T, J):
 
     def calc():
         probs = random_uniform()
-        if (probs < math.exp(2*J[0]*spin[z, x, y]*sum_nn()/T[n,l])):
+        if (probs < math.exp(2*(J[0]*sum_nn())*spin[z, x, y]/T[n,l])):
             spin[z, x, y] *= np.int8(-1)
 
     if (p == 0 and q == 1) or (p == 1 and q == 0):
@@ -195,4 +195,74 @@ def update_green (spin, seed, T, J):
             spin[z, x, y] *= np.int8(-1)
 
     if (p == 2 and q == 0) or (p == 0 and q == 1):
+        calc()
+
+import math
+import numpy as np
+from numba import cuda
+
+@cuda.jit
+def update_black_ext (spin, seed, T, J, h):
+    m = T.shape[1]
+    z, x, y = cuda.grid(3)
+    n = int(math.floor (z / m))
+    l = z % m
+    p, q = x % 2, y % 2
+
+    def random_uniform():
+        seed[z, x, y] = np.int32((seed[z ,x, y]*1664525 + 1013904223) % 2**31)
+        return seed[z, x, y] / (2**31)
+
+    def bvc (x):
+        if x == spin.shape[1]:
+            x = 0
+        return x       
+
+    def sum_nn():
+        value = 0
+        value += spin[z, x, bvc(y+1)]
+        value += spin[z, x, y-1]
+        value += spin[z, bvc(x+1), y]
+        value += spin[z, x-1, y]
+        return value
+
+    def calc():
+        probs = random_uniform()
+        if (probs < math.exp(2*(J[0]*sum_nn() - h[0])*spin[z, x, y]/T[n,l])):
+            spin[z, x, y] *= np.int8(-1)
+
+    if (p == 0 and q == 0) or (p == 1 and q == 1):
+        calc()
+
+@cuda.jit
+def update_white_ext (spin, seed, T, J, h):
+    m = T.shape[1]
+    z, x, y = cuda.grid(3)
+    n = int(math.floor (z / m))
+    l = z % m
+    p, q = x % 2, y % 2
+
+    def random_uniform():
+        seed[z, x, y] = np.int32((seed[z ,x, y]*1664525 + 1013904223) % 2**31)
+        return seed[z, x, y] / (2**31)
+
+    def bvc (x):
+        if x == spin.shape[1]:
+            x = 0
+        return x       
+
+    def sum_nn():
+        value = 0
+        value += spin[z, x, bvc(y+1)]
+        value += spin[z, x, y-1]
+        value += spin[z, bvc(x+1), y]
+        value += spin[z, x-1, y]
+        return value
+
+    def calc():
+        probs = random_uniform()
+        if (probs < math.exp(2*(J[0]*sum_nn()-h[0])*spin[z, x, y]/T[n,l])):
+            spin[z, x, y] *= np.int8(-1)
+
+    if (p == 0 and q == 1) or (p == 1 and q == 0):
         calc()
